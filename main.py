b9512f3a9784ef907cab82772a6834582ae47538
Hriday Keswani <hridaykeswani@gmail.com>
2023-11-15 14:10:57 +0530

    implement loss functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sasta_tensorflow import layers,activations
+from sasta_tensorflow import layers,activations,loss
 
 input = [1,2,3,4,115]
 
@@ -7,5 +7,5 @@ dense1 = layers.Dense(5,5)
 act1 = activations.softmax()
 dense1.forward(input=input)
 act1.forward(dense1.output)
-
+l = loss.categorical_cross_entropy()
 print(act1.output)
